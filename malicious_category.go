0e5a051311e17686832964f1cca93a4c638979f8
RJ Nanjegowda <rnanjegowda@zvelo.com>
2016-09-02 11:43:03 -0600

    removed MD categories from category

--- malicious_category.go
+++ malicious_category.go
@@ -2,13 +2,13 @@ package msg
 
 // MaliciousCategoryLong is a map from malicious category to long string names
 var MaliciousCategoryLong = map[MaliciousCategory]string{
-	MaliciousCategory_BOTS:    "Botnet",
-	MaliciousCategory_CANDC:   "Command and Control Centers",
-	MaliciousCategory_COMPR:   "Compromised & Links To Malware",
-	MaliciousCategory_MALHOME: "Malware Call-Home",
-	MaliciousCategory_MAL:     "Malware Distribution Point",
-	MaliciousCategory_FRAUD:   "Phishing/Fraud",
-	MaliciousCategory_SPAM:    "Spam URLs",
-	MaliciousCategory_SPYWARE: "Spyware & Questionable Software",
-	MaliciousCategory_UNK:     "Unknown",
+	MaliciousCategory_BOTS_4:    "Botnet",
+	MaliciousCategory_CANDC_4:   "Command and Control Centers",
+	MaliciousCategory_COMPR_4:   "Compromised & Links To Malware",
+	MaliciousCategory_MALHOME_4: "Malware Call-Home",
+	MaliciousCategory_MAL_4:     "Malware Distribution Point",
+	MaliciousCategory_FRAUD_4:   "Phishing/Fraud",
+	MaliciousCategory_SPAM_4:    "Spam URLs",
+	MaliciousCategory_SPYWARE_4: "Spyware & Questionable Software",
+	MaliciousCategory_UNK:       "Unknown",
 }
