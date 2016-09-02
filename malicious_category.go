09e89c162cb3c0be2871c4c866572ce43bca4652
RJ Nanjegowda <rnanjegowda@zvelo.com>
2016-09-02 11:24:42 -0600

    enum names

--- malicious_category.go
+++ malicious_category.go
@@ -2,13 +2,13 @@ package msg
 
 // MaliciousCategoryLong is a map from malicious category to long string names
 var MaliciousCategoryLong = map[MaliciousCategory]string{
-	MaliciousCategory_T_BOTS_4:    "Botnet",
-	MaliciousCategory_T_CANDC_4:   "Command and Control Centers",
-	MaliciousCategory_T_COMPR_4:   "Compromised & Links To Malware",
-	MaliciousCategory_T_MALHOME_4: "Malware Call-Home",
-	MaliciousCategory_T_MAL_4:     "Malware Distribution Point",
-	MaliciousCategory_T_FRAUD_4:   "Phishing/Fraud",
-	MaliciousCategory_T_SPAM_4:    "Spam URLs",
-	MaliciousCategory_T_SPYWARE_4: "Spyware & Questionable Software",
-	MaliciousCategory_T_UNKNOWN:   "Unknown Category",
+	MaliciousCategory_BOTS:    "Botnet",
+	MaliciousCategory_CANDC:   "Command and Control Centers",
+	MaliciousCategory_COMPR:   "Compromised & Links To Malware",
+	MaliciousCategory_MALHOME: "Malware Call-Home",
+	MaliciousCategory_MAL:     "Malware Distribution Point",
+	MaliciousCategory_FRAUD:   "Phishing/Fraud",
+	MaliciousCategory_SPAM:    "Spam URLs",
+	MaliciousCategory_SPYWARE: "Spyware & Questionable Software",
+	MaliciousCategory_UNK:     "Unknown Category",
 }
