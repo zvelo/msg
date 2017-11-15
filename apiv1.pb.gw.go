9e26128bd3b7fe1dbf272993c1e49dde400b7733
Joshua Rubin <jrubin@zvelo.com>
2017-11-15 16:00:53 -0700

    add stream endpoint
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- apiv1.pb.gw.go
+++ apiv1.pb.gw.go
@@ -13,6 +13,7 @@ import (
 	"net/http"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/grpc-ecosystem/grpc-gateway/utilities"
 	"golang.org/x/net/context"
@@ -78,6 +79,23 @@ func request_APIv1_Suggest_0(ctx context.Context, marshaler runtime.Marshaler, c
 
 	msg, err := client.Suggest(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
+
+}
+
+func request_APIv1_Stream_0(ctx context.Context, marshaler runtime.Marshaler, client APIv1Client, req *http.Request, pathParams map[string]string) (APIv1_StreamClient, runtime.ServerMetadata, error) {
+	var protoReq empty.Empty
+	var metadata runtime.ServerMetadata
+
+	stream, err := client.Stream(ctx, &protoReq)
+	if err != nil {
+		return nil, metadata, err
+	}
+	header, err := stream.Header()
+	if err != nil {
+		return nil, metadata, err
+	}
+	metadata.HeaderMD = header
+	return stream, metadata, nil
 
 }
 
@@ -206,6 +224,35 @@ func RegisterAPIv1HandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_APIv1_Stream_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_APIv1_Stream_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_APIv1_Stream_0(ctx, mux, outboundMarshaler, w, req, func() (proto.Message, error) { return resp.Recv() }, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -215,6 +262,8 @@ var (
 	pattern_APIv1_Result_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "query", "request_id"}, ""))
 
 	pattern_APIv1_Suggest_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "suggest"}, ""))
+
+	pattern_APIv1_Stream_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "stream"}, ""))
 )
 
 var (
@@ -223,4 +272,6 @@ var (
 	forward_APIv1_Result_0 = runtime.ForwardResponseMessage
 
 	forward_APIv1_Suggest_0 = runtime.ForwardResponseMessage
+
+	forward_APIv1_Stream_0 = runtime.ForwardResponseStream
 )
